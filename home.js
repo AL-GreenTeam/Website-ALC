af808eef010a184c0ec619c79b64dee7d7857927
Margaret Maina <captanomaina@gmail.com>
2020-08-15 00:08:16 +0400

    making buttons responsive

--- home.js
+++ home.js
@@ -14,7 +14,7 @@ let writings_texts = [
 
     new Texts(
         "Welcome to The African Leadership University",
-        "Join a group of innovative young African leaders in solving some of Africa's challenges ",
+        "Join  a group of innovative african leaders ready to become change makers. We are an institution aimed at creating leaders with the ability to influence and bring change to the continent. We train our students to tackle our seven global challenges which include Urbanization, Education, Healthcare, Infrastructure, Job Creation, Governance and Climate Change. ",
         "https://www.youtube.com/embed/Paap6LOeGzc"),
 
     new Texts(
@@ -24,7 +24,7 @@ let writings_texts = [
 
     new Texts(
         "ALC Mauritius Campus",
-        "Get a preview of our campus infrastructure and the beautiful Island of Mauritius",
+        "Our campus located on the island paradise of Mauritius provides a serene environment to our future leaders for learning and is located in close vicinity to the island capital of Port Louis where our students can experience the rich culture of the native citizens",
         "https://www.youtube.com/embed/3ES0lurL03M")
     
         
@@ -32,53 +32,83 @@ let writings_texts = [
 
 let slideshow_index = 0;
 let cloned_node = null;
-let is_handled = false;
+let handled_objects = 0;
 let intervalId = null;
-function moveRight() {
-    videos.classList.add("moveright");
-    writings.classList.add("moveright");
-    is_handled = false;
+function move(isLeft) {
+    let direction_class = "moveright";
+    let index = slideshow_index - 1;
+    if (isLeft) {
+        direction_class = "moveleft";
+        index = slideshow_index +1;
+    }
+
+    videos.classList.add(direction_class);
+    writings.classList.add(direction_class);
+    handled_objects = 0;
     cloned_node = videos.querySelector("iframe").cloneNode(true);
-    let index = slideshow_index + 1;
+    
 
     if (index == writings_texts.length){
         index = 0;
+    } else if (index < 0) {
+        index = writings_texts.length - 1; 
     }
+
     let next_text = writings_texts[index];
     cloned_node.setAttribute("src", next_text.youtube_link);
 }
+
+let animation_IntervalId = null;
+function set_Interval() {
+    animation_IntervalId = window.setInterval(move,20000, true);
+}
+
 function setup(event) {
     if (intervalId) {
         window.clearInterval(intervalId);
         intervalId = null;
 
-        window.setInterval(moveRight, 20000);
+        set_Interval();
     }
 }
 function setHidden(event) {
     let element = event.currentTarget;
-    if (element.className.includes("moveright")) {
-       element.classList.remove("moveright");
+    let className = element.className;
+    if (className.includes("moveleft") || className.includes("moveright")) {
+        handled_objects++ ;
+        let fromLeft = false;
+        if (element.className.includes("moveleft")) {
+            element.classList.remove("moveleft");
+        }else if (element.className.includes("moveright")) {
+            element.classList.remove("moveright");
+            fromLeft = true;
+        }
 
-       if (!is_handled) {
-            is_handled = true;
-            slideshow_index++;
+        if (handled_objects === 1) {
+            slideshow_index += (fromLeft) ? -1 : 1;
             if (slideshow_index == writings_texts.length) {
-                slideshow_index = 0;
-                
+                slideshow_index = 0;   
+            } else if (slideshow_index < 0) {
+                slideshow_index = writings_texts.length -1; 
             }
             let text = writings_texts[slideshow_index];
             writings.querySelector("h1").textContent = text.header;
             writings.querySelector("p").textContent = text.paragraph;
 
             videos.querySelector("iframe").replaceWith(cloned_node);
+        }
 
-       }
-
-       element.classList.add("appear");
-    }  
-    else if (element.className.includes("appear")){
-        element.classList.remove("appear");
+        
+        if (fromLeft) {
+            element.classList.add("appearfromleft"); 
+        } else {
+            element.classList.add("appearfromright");
+        }
+       
+    } else if (element.className.includes("appearfromleft")){
+        element.classList.remove("appearfromleft");
+    }else if (element.className.includes("appearfromright")){
+        element.classList.remove("appearfromright");
     }
 }
 
@@ -90,7 +120,7 @@ videos.addEventListener("animationstart", setup, true);
 let writings = document.getElementsByClassName("writings")[0];
 writings.addEventListener("animationend", setHidden, true);
 
-intervalId = window.setInterval(moveRight, 10000);
+intervalId = window.setInterval(move, 10000, true);
 
 let searchIsVisible = false;
 document.getElementById("srch").addEventListener("click", () =>{
@@ -101,4 +131,16 @@ document.getElementById("srch").addEventListener("click", () =>{
     }
     searchIsVisible = !searchIsVisible;
     input.style.display = value;
+});
+
+document.getElementById("left_btn").addEventListener("click", () =>{
+    window.clearInterval(animation_IntervalId);
+    move(true);
+    set_Interval();
+});
+
+document.getElementById("right_btn").addEventListener("click", () =>{
+    window.clearInterval(animation_IntervalId);
+    move(false);
+    set_Interval();
 });
